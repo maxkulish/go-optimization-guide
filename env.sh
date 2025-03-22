6897e1434d38d8c807e98ca5f4a9bb57add2e1e7
Alexander Stavonin <a.stavonin@gmail.com>
2025-03-22 19:07:14 +0600

    prepare working environment script

---
+++ env.sh
@@ -0,0 +1,6 @@
+python3 -m venv venv
+
+source ./venv/bin/activate
+export PATH=`pwd`/venv/bin:$PATH
+
+pip install mkdocs mkdocs-material mkdocs-git-revision-date-localized-plugin mkdocs-include-markdown-plugin
